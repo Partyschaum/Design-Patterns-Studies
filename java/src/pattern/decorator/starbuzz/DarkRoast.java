9cb9a8c1a751b000fe4dcfb6fa90d11d8cca6d0e
Hauke Stange <drei.und.zwanzig@web.de>
2012-01-15 01:24:10 +0100

    Some code examples for the Decorator Pattern.

---
+++ java/src/pattern/decorator/starbuzz/DarkRoast.java
@@ -0,0 +1,14 @@
+package pattern.decorator.starbuzz;
+
+public class DarkRoast extends Beverage
+{
+    public DarkRoast()
+    {
+	description = "DarkRoast";
+    }
+
+    public double cost()
+    {
+	return 0.99;
+    }
+}
